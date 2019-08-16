737bdb1add7bda49c299bf60f9dd86e7acd86710
JumpTigerWong <38396114+JumpTigerWong@users.noreply.github.com>
2019-08-16 16:01:04 +0800

    将有序数组转换为二叉搜索树
    leetCode算法题将有序数组转换为二叉搜索树，递归实现

---
+++ sortedArrayToBST.java
@@ -0,0 +1,14 @@
+public TreeNode sortedArrayToBST(int[] nums) {
+		return sortedArrayToBST(nums,0,nums.length);
+	}
+	
+	public TreeNode sortedArrayToBST(int []nums,int start,int end) {
+		if(end==start) {
+			return null;
+		}
+		int mid=(start+end)/2;
+		TreeNode root=new TreeNode(nums[mid]);
+		root.left=sortedArrayToBST(nums,start,mid);
+		root.right=sortedArrayToBST(nums,mid+1,end);
+		return root;
+	}
