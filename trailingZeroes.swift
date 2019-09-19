e88c34d2f2825ce3655fb48a0d1191f233b1e49e
JumpTigerWong <38396114+JumpTigerWong@users.noreply.github.com>
2019-09-19 20:39:43 +0800

    阶乘0的个数
    leetCode算法题阶乘0的个数，没怎么搞懂，使用swift实现，第一次使用swift写程序

---
+++ trailingZeroes.swift
@@ -0,0 +1,11 @@
+class Solution{
+    func trailingZeroes(_ n:Int)->Int{
+        var res=0;
+        var temp=n
+        while temp>0{
+            temp=temp/5
+            res+=n
+        }
+        return res
+    }
+}
